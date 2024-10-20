3a0935288069240369ef05c71ebf269c01b963d5
ABDUL SHARIQ KALIM <shariqkalim123@gmail.com>
2024-10-21 01:45:08 +0530

    Final1

--- api/app.py
+++ api/app.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# Function to fetch the stock price from Google Finance
 def get_stock_price(ticker="ZOMATO"):
     url = f'https://www.google.com/finance/quote/{ticker}:NSE'
     response = requests.get(url)
@@ -13,15 +12,14 @@ def get_stock_price(ticker="ZOMATO"):
     ans = float(soup.find(class_=myClass).text.strip()[1:].replace(',', ''))
     return ans
 
-# Route to serve the web page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# API route to fetch the latest stock price
 @app.route('/get_stock_price')
 def get_price():
     price = get_stock_price()
     return jsonify({'price': price})
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
